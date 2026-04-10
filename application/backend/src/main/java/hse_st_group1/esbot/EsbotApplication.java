dc4f693e0bf4884e7a7cb4e3864dd28ad0644454
hendrik <207308842+HNKBKR@users.noreply.github.com>
2026-04-10 12:50:27 +0200

    feat: Added backend skeleton

---
+++ application/backend/src/main/java/hse_st_group1/esbot/EsbotApplication.java
@@ -0,0 +1,13 @@
+package hse_st_group1.esbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsbotApplication.class, args);
+	}
+
+}
---
+++ application/backend/src/test/java/hse_st_group1/esbot/EsbotApplicationTests.java
@@ -0,0 +1,31 @@
+package hse_st_group1.esbot;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.junit.jupiter.api.Test;
+
+@SpringBootTest
+class EsbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
+@SpringBootTest
+class SmokeTest {
+
+	@Autowired
+	private TestController controller;
+
+	@Test
+	void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
+	}
+}

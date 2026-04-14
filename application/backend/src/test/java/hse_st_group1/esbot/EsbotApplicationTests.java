160149298da65f894a51caecce43226a0506089d
hendrik <207308842+HNKBKR@users.noreply.github.com>
2026-04-15 00:39:26 +0200

    fix: Added H2 as database for testing and eliminating the postgres requirement for testing

--- application/backend/src/test/java/hse_st_group1/esbot/EsbotApplicationTests.java
+++ application/backend/src/test/java/hse_st_group1/esbot/EsbotApplicationTests.java
@@ -6,10 +6,13 @@ import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import hse_st_group1.esbot.controller.TestController;
 
+
 @SpringBootTest
+@ActiveProfiles("test")
 class EsbotApplicationTests {
 
 	@Test
@@ -17,7 +20,10 @@ class EsbotApplicationTests {
 	}
 
 }
+
+
 @SpringBootTest
+@ActiveProfiles("test")
 class SmokeTest {
 
 	@Autowired

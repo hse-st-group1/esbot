cdaabd61c9cc5213d8d1611461edf952fa4286fa
hendrik <207308842+HNKBKR@users.noreply.github.com>
2026-04-19 12:33:08 +0200

    chore: removed unused imports

--- application/backend/src/main/java/hse_st_group1/esbot/repository/QuizRequestRepository.java
+++ application/backend/src/main/java/hse_st_group1/esbot/repository/QuizRequestRepository.java
@@ -3,9 +3,11 @@ package hse_st_group1.esbot.repository;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import hse_st_group1.esbot.model.QuizRequest;
 
+@Repository
 public interface QuizRequestRepository extends JpaRepository<QuizRequest, UUID> {
 
 }
--- application/backend/src/test/java/hse_st_group1/esbot/EsbotApplicationTests.java
+++ application/backend/src/test/java/hse_st_group1/esbot/EsbotApplicationTests.java
@@ -3,7 +3,6 @@ package hse_st_group1.esbot;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;

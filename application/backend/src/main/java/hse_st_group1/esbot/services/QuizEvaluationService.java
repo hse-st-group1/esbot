eab251f6e97bb446244057ec3e1ed94d4bd98b1f
dev <207308842+HNKBKR@users.noreply.github.com>
2026-05-25 13:08:40 +0200

    feat: added aiservice check to QuizEvaluationService

--- application/backend/src/main/java/hse_st_group1/esbot/services/QuizEvaluationService.java
+++ application/backend/src/main/java/hse_st_group1/esbot/services/QuizEvaluationService.java
@@ -2,28 +2,36 @@ package hse_st_group1.esbot.services;
 
 import org.springframework.stereotype.Service;
 
+import hse_st_group1.esbot.AIServiceUnavailableException;
 import hse_st_group1.esbot.model.QuizAnswer;
 import hse_st_group1.esbot.model.QuizEvaluation;
 import hse_st_group1.esbot.repository.QuizAnswerRepository;
 import hse_st_group1.esbot.repository.QuizEvaluationRepository;
 import jakarta.transaction.Transactional;
-import lombok.RequiredArgsConstructor;
 
 
 
 @Service
-@RequiredArgsConstructor
 public class QuizEvaluationService {
 
     public final QuizAnswerRepository quizAnswerRepository;
     public final AIService aiService;
     public final QuizEvaluationRepository quizEvaluationRepository;
 
-    @Transactional
-    public QuizEvaluation evaluate(QuizAnswer answer) {
-        String evaluation = aiService.evaluateAnswer(answer.getAnswer());
-        QuizEvaluation quizEvaluation = new QuizEvaluation(null, answer.getQuizItem(), answer, evaluation);
-        quizEvaluationRepository.save(quizEvaluation);
-        return quizEvaluation;
+    public QuizEvaluationService(QuizAnswerRepository quizAnswerRepository, AIService aiService, QuizEvaluationRepository quizEvaluationRepository){
+        this.quizAnswerRepository = quizAnswerRepository;
+        this.aiService = aiService;
+        this.quizEvaluationRepository = quizEvaluationRepository;
+    }
+
+    public void evaluate(QuizAnswer answer) {
+        if(aiService.isAvailable()){
+            String evaluation = aiService.evaluateAnswer(answer.getAnswer());
+            QuizEvaluation quizEvaluation = new QuizEvaluation(null, answer.getQuizItem(), answer, evaluation);
+            quizEvaluationRepository.save(quizEvaluation);
+        }
+        else{
+            throw new AIServiceUnavailableException("Error: Evaluation service is currently unavailable");
+        }
     }
 }

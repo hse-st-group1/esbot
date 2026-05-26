b2f28aff3f3b8b3b76aeaa6792e50b1d20401c6f
mint <207308842+HNKBKR@users.noreply.github.com>
2026-05-26 15:31:34 +0200

    feat: added message history retrieval to Message repository

--- application/backend/src/main/java/hse_st_group1/esbot/repository/MessageRepository.java
+++ application/backend/src/main/java/hse_st_group1/esbot/repository/MessageRepository.java
@@ -1,11 +1,13 @@
 package hse_st_group1.esbot.repository;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import hse_st_group1.esbot.model.Message;
+import hse_st_group1.esbot.model.Session;
 
 public interface MessageRepository extends JpaRepository<Message, UUID> {
-
+    List<Message> findBySessionOrderByTimestamp(Session session);
 }

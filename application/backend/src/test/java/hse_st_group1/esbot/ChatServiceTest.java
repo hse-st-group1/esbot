46197977a2d2569c8cd4dd7495e0c81131eaa414
mint <207308842+HNKBKR@users.noreply.github.com>
2026-05-26 10:19:25 +0200

    feat: added new test cases to ChatServiceTest

--- application/backend/src/test/java/hse_st_group1/esbot/ChatServiceTest.java
+++ application/backend/src/test/java/hse_st_group1/esbot/ChatServiceTest.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -12,8 +14,11 @@ import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import hse_st_group1.esbot.model.Message;
+import hse_st_group1.esbot.model.QuizEvaluation;
+import hse_st_group1.esbot.model.QuizRequest;
 import hse_st_group1.esbot.model.Session;
 import hse_st_group1.esbot.model.User;
+import hse_st_group1.esbot.model.QuizRequest.Difficulty;
 import hse_st_group1.esbot.repository.MessageRepository;
 import hse_st_group1.esbot.repository.QuizAnswerRepository;
 import hse_st_group1.esbot.repository.QuizEvaluationRepository;
@@ -64,7 +69,7 @@ class ChatServiceTest {
     }
 
     @Test
-    void sendMessageTestAIServiceNotAvailable(){
+    void sendMessageTestAIServiceNotAvailableTest(){
         String messageContent = "Hello AI";
         ChatService chatService = new ChatService(sessionRepository, messageRepository, aiService, quizItemRepository, quizRequestRepository, quizAnswerRepository, quizEvaluationRepository);
         this.session = chatService.createNewSession(user);
@@ -81,7 +86,7 @@ class ChatServiceTest {
     }
 
     @Test
-    void sendMessageTestAIServiceAvailable(){
+    void sendMessageTestAIServiceAvailableTest(){
         String messageContent = "Hello AI";
         ChatService chatService = new ChatService(sessionRepository, messageRepository, aiService, quizItemRepository, quizRequestRepository, quizAnswerRepository, quizEvaluationRepository);
         this.session = chatService.createNewSession(user);
@@ -97,5 +102,100 @@ class ChatServiceTest {
         }
     }
 
-    
+    @Test
+    void sendQuizRequestAIServiceAvailableTest(){
+        String quizTopic = "Topic: Software Testing";
+        ChatService chatService = new ChatService(sessionRepository, messageRepository, aiService, quizItemRepository, quizRequestRepository, quizAnswerRepository, quizEvaluationRepository);
+        this.session = chatService.createNewSession(user);
+        Mockito.when(aiService.isAvailable()).thenReturn(true);
+        Mockito.when(aiService.createQuestions(Mockito.anyString())).thenReturn(List.of("Question 1: How Many Test Questions?", "Question 2: How Hard Are The Test Questions?", "Question 3: What Is A Rabbyte?"));
+        QuizRequest request = null;
+        try{
+            request = chatService.sendQuizRequest(quizTopic, 3, Difficulty.EASY);
+        }
+        catch(AIServiceUnavailableException aiServiceUnavailableException){
+            assertNull(aiServiceUnavailableException);
+        }
+        assertNotNull(request);
+    }
+
+    @Test
+    void sendQuizRequestAIServiceNotAvailableTest(){
+        String quizTopic = "Topic: Software Testing";
+        ChatService chatService = new ChatService(sessionRepository, messageRepository, aiService, quizItemRepository, quizRequestRepository, quizAnswerRepository, quizEvaluationRepository);
+        this.session = chatService.createNewSession(user);
+        Mockito.when(aiService.isAvailable()).thenReturn(false);
+        Mockito.when(aiService.createQuestions(Mockito.anyString())).thenReturn(List.of("Question 1: How Many Test Questions?", "Question 2: How Hard Are The Test Questions?", "Question 3: What Is A Rabbyte?"));
+        QuizRequest request = null;
+        try{
+            request = chatService.sendQuizRequest(quizTopic, 3, Difficulty.EASY);
+        }
+        catch(AIServiceUnavailableException aiServiceUnavailableException){
+            assertNotNull(aiServiceUnavailableException);
+            assertEquals("Error: Quiz service is currently unavailable", aiServiceUnavailableException.getMessage());
+        }
+        assertNull(request);
+    }
+
+    @Test
+    void sendQuizRequestWithoutTopicTest(){
+        String quizTopic = "";
+        ChatService chatService = new ChatService(sessionRepository, messageRepository, aiService, quizItemRepository, quizRequestRepository, quizAnswerRepository, quizEvaluationRepository);
+        this.session = chatService.createNewSession(user);
+        Mockito.when(aiService.isAvailable()).thenReturn(true);
+        Mockito.when(aiService.createQuestions(Mockito.anyString())).thenReturn(List.of("Question 1: How Many Test Questions?", "Question 2: How Hard Are The Test Questions?", "Question 3: What Is A Rabbyte?"));
+        QuizRequest request = null;
+        try{
+            request = chatService.sendQuizRequest(quizTopic, 3, Difficulty.EASY);
+        }
+        catch(NoQuizTopicProvidedException noQuizTopicProvidedException){
+            assertNotNull(noQuizTopicProvidedException);
+            assertEquals("Error: No quiz topic provided.", noQuizTopicProvidedException.getMessage());
+        }
+        assertNull(request);
+    }
+
+    @Test
+    void evaluateAnswerAIServiceAvailableTest(){
+        ChatService chatService = new ChatService(sessionRepository, messageRepository, aiService, quizItemRepository, quizRequestRepository, quizAnswerRepository, quizEvaluationRepository);
+        this.session = chatService.createNewSession(user);
+        QuizRequest request = null;
+        QuizEvaluation evaluation = null;
+        Mockito.when(aiService.isAvailable()).thenReturn(true);
+        Mockito.when(aiService.createQuestions(Mockito.anyString())).thenReturn(List.of("Question 1: How Many Test Questions?", "Question 2: How Hard Are The Test Questions?", "Question 3: What Is A Rabbyte?"));
+        Mockito.when(aiService.evaluateAnswer(Mockito.anyString())).thenReturn("correct");
+        request = chatService.sendQuizRequest("Topic: Test", 3, Difficulty.EASY);
+        try{
+            evaluation = chatService.receiveEvaluation("Test", request.getQuizItems().get(0));
+        }
+        catch(AIServiceUnavailableException aiServiceUnavailableException){
+            assertNull(aiServiceUnavailableException);
+        }
+        assertNotNull(request);
+        assertNotNull(evaluation);
+        assertEquals("correct", evaluation.getEvaluation());
+    }
+
+    @Test
+    void evaluateAnswerAIServiceNotAvailableTest(){
+        ChatService chatService = new ChatService(sessionRepository, messageRepository, aiService, quizItemRepository, quizRequestRepository, quizAnswerRepository, quizEvaluationRepository);
+        this.session = chatService.createNewSession(user);
+        QuizRequest request = null;
+        QuizEvaluation evaluation = null;
+        Mockito.when(aiService.isAvailable()).thenReturn(true);
+        Mockito.when(aiService.createQuestions(Mockito.anyString())).thenReturn(List.of("Question 1: How Many Test Questions?", "Question 2: How Hard Are The Test Questions?", "Question 3: What Is A Rabbyte?"));
+        Mockito.when(aiService.evaluateAnswer(Mockito.anyString())).thenReturn("correct");
+        request = chatService.sendQuizRequest("Topic: Test", 3, Difficulty.EASY);
+        Mockito.when(aiService.isAvailable()).thenReturn(false);
+        try{
+            evaluation = chatService.receiveEvaluation("Test", request.getQuizItems().get(0));
+        }
+        catch(AIServiceUnavailableException aiServiceUnavailableException){
+            assertNotNull(aiServiceUnavailableException);
+            assertEquals("Error: Evaluation service is currently unavailable", aiServiceUnavailableException.getMessage());
+        }
+        assertNotNull(request);
+        assertNull(evaluation);
+    }
+
 }

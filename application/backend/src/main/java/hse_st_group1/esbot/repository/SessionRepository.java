bcd463c75c1d1ff94739b66c63b1db7ffbfa633c
mint <207308842+HNKBKR@users.noreply.github.com>
2026-05-26 15:32:24 +0200

    feat: added retrieval functions to session repository

--- application/backend/src/main/java/hse_st_group1/esbot/repository/SessionRepository.java
+++ application/backend/src/main/java/hse_st_group1/esbot/repository/SessionRepository.java
@@ -1,12 +1,14 @@
 package hse_st_group1.esbot.repository;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import hse_st_group1.esbot.model.Session;
+import hse_st_group1.esbot.model.User;
 
 public interface SessionRepository extends JpaRepository<Session, UUID> {
-
-    
+    Session findBySessionID(UUID sessionID);
+    List<Session> findByUser(User user);
 }

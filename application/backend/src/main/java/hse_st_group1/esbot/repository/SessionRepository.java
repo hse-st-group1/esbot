616568c801ced655fc390ae07a668dffe44ff1eb
jaboit02 <jaboit02@hs-esslingen.de>
2026-06-01 20:11:36 +0200

    review 8.3: Added comments to explain why not all functions were expicitly implemented

--- application/backend/src/main/java/hse_st_group1/esbot/repository/SessionRepository.java
+++ application/backend/src/main/java/hse_st_group1/esbot/repository/SessionRepository.java
@@ -9,6 +9,11 @@ import hse_st_group1.esbot.model.Session;
 import hse_st_group1.esbot.model.User;
 
 public interface SessionRepository extends JpaRepository<Session, UUID> {
+    // Creating a new session is inherited from JpaRepository as save() 
     Session findBySessionID(UUID sessionID);
     List<Session> findByUser(User user);
+    // Appending a new message to a session is handled by SessionService
+    // Retrieving the full message history of a session is handled by MessageRepository
+    // Updating session metadata is handled by save()
+    // Deleting a session is inherited from JpaRepository as deleteByID(UUID sessionID)
 }

02044cb7c71d2644f1c7c1b1f5154090b5786ef3
hendrik <207308842+HNKBKR@users.noreply.github.com>
2026-06-04 15:58:50 +0200

    chore: adjusted pom.xml in preperation for git pre-commit hook and removed unused import

--- application/backend/src/test/java/hse_st_group1/esbot/QuizItemTest.java
+++ application/backend/src/test/java/hse_st_group1/esbot/QuizItemTest.java
@@ -1,7 +1,6 @@
 package hse_st_group1.esbot;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.Set;
 import java.util.UUID;

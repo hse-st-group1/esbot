73956e92abfd1778570eacd921012ee65224588c
hendrik <207308842+HNKBKR@users.noreply.github.com>
2026-06-18 11:08:42 +0200

    feat: added some material UI components for testing

--- application/frontend/ESbot/src/app/components/message-input/message-input.ts
+++ application/frontend/ESbot/src/app/components/message-input/message-input.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-message-input',
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './message-input.html',
   styleUrl: './message-input.css',
 })

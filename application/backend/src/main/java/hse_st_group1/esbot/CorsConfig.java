03f4a78bce171f69205d2418fffff65fddebe1d7
hendrik <207308842+HNKBKR@users.noreply.github.com>
2026-06-19 11:58:56 +0200

    feat: upgraded spring from 4.0.6 to 4.0.7

--- application/backend/src/main/java/hse_st_group1/esbot/CorsConfig.java
+++ application/backend/src/main/java/hse_st_group1/esbot/CorsConfig.java
@@ -6,6 +6,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 

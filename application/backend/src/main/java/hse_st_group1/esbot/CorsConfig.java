bdffbbfe3efba4790242863e28606b38ee9cefcb
jaboit02 <jaboit02@hs-esslingen.de>
2026-06-19 14:07:29 +0200

    chore: adjusted cors config to not trigger pmd linter

--- application/backend/src/main/java/hse_st_group1/esbot/CorsConfig.java
+++ application/backend/src/main/java/hse_st_group1/esbot/CorsConfig.java
@@ -6,11 +6,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:4200")
                 .allowedMethods("*")
